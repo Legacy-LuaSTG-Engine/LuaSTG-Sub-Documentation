de416d4fc726d666b03b26a471d5057bb76d4749
璀境石 <Kuanlan@outlook.com>
2023-06-17 15:35:11 +0800

    额外的API

--- experiment/lstg/ResourceSprite.lua
+++ experiment/lstg/ResourceSprite.lua
@@ -2,4 +2,13 @@
 ---@class lstg.ResourceSprite
 local M = {}
 
+---@param x number
+---@param y number
+function M:setCenter(x, y)
+end
+
+---@param v number
+function M:setUnitsPerPixel(v)
+end
+
 return M
