4fadc5fe407b60ba1b63d1db0fac99b853641edd
璀境石 <git@chuikingshek.com>
2024-09-26 14:43:00 +0800

    [WIP] daydream 17

--- experiment/lstg/Window.lua
+++ experiment/lstg/Window.lua
@@ -1,25 +1,75 @@
 ---@diagnostic disable: missing-return
 
+--------------------------------------------------------------------------------
+--- 类
+--- Class
+
 ---@class lstg.Window
 local M = {}
 
 --------------------------------------------------------------------------------
---- window setting
+--- 窗口信息
+--- Window info
 
----@param text string
-function M:setTitle(text)
-end
-
+--- 获取当前窗口客户区尺寸（像素）  
+---   
+--- Get current client-area size (in pixels)  
 ---@return lstg.Display.Size size
 function M:getClientAreaSize()
 end
 
+--- 获取当前窗口框体样式，初始值为 `lstg.Window.FrameStyle.normal`  
+--- 注意：全屏模式下返回的是上次设置的样式，而不是当前窗口的样式（无边框模式）  
+---   
+--- Get the current Window Decorations style, initial value is `lstg.Window.FrameStyle.normal`  
+--- Note: Fullscreen mode returns the last style set by `lstg.Window.setWindowed`
+--- or other APIs, not the current window's style (borderless)  
+---@see lstg.Window.setWindowed
 ---@return lstg.Window.FrameStyle style
 function M:getStyle()
 end
 
+---@see lstg.Display.getDisplayScale
 ---@return number scale
 function M:getDisplayScale()
+end
+
+--------------------------------------------------------------------------------
+--- 窗口设置
+--- Window setup
+
+--- 设置窗口标题栏文本  
+--- 
+--- Modify the text of the window TitleBar  
+---@param text string
+function M:setTitle(text)
+end
+
+--- 进入窗口模式  
+--- 参数 `width` 和 `height` 决定了窗口客户区的尺寸（像素）  
+--- 可选的参数 `style` 用于修改窗口框体样式  
+--- 可选的参数 `display` 用于指定窗口位于哪个显示器  
+---   
+--- Enter windowed mode  
+--- The parameter `width` and `height` specify the client-area size (in pixels)  
+--- The optional parameter `style` is used to modify Window Decorations style  
+--- The optional parameter `display` is used to specify the Display
+--- in which the window is located  
+---@param width number
+---@param height number
+---@param style lstg.Window.FrameStyle?
+---@param display lstg.Display?
+function M:setWindowed(width, height, style, display)
+end
+
+--- 进入全屏模式  
+--- 可选的参数 `display` 用于指定窗口位于哪个显示器  
+---   
+--- Enter fullscreen mode  
+--- The optional parameter `display` is used to specify the Display
+--- in which the window is located  
+---@param display lstg.Display?
+function M:setFullscreen(display)
 end
 
 --------------------------------------------------------------------------------
@@ -46,26 +96,24 @@ end
 --]]
 
 --------------------------------------------------------------------------------
---- window placement
+--- 拓展
+--- Extension
 
----@param display lstg.Display?
-function M:centered(display)
-end
-
----@param width number
----@param height number
----@param style lstg.Window.FrameStyle?
----@param display lstg.Display?
-function M:setWindowed(width, height, style, display)
-end
-
----@param display lstg.Display?
-function M:setFullscreen(display)
-end
-
---------------------------------------------------------------------------------
---- extension
-
+--- 查询特定的拓展接口是否可用，接口不存在或者不支持将返回 nil  
+---   
+--- Query whether a specific extention interface is available,
+--- return `nil` if the interface does not exist or is not supported  
+---   
+--- 例如 | Example:  
+---   
+--- ```lua
+--- local Window = require("lstg.Window")
+--- local main_window = Window.getMain()
+--- local extension = main_window:queryInterface("lstg.Window.Windows11Extension")
+--- if extension then
+---     -- todo...
+--- end
+--- ```
 ---@generic T
 ---@param name `T`
 ---@return T?
@@ -73,18 +121,27 @@ function M:queryInterface(name)
 end
 
 --------------------------------------------------------------------------------
---- extension: Windows 11 window corner
+--- 拓展：Windows 11 相关拓展
+--- Extension: Windows 11
 
 ---@class lstg.Window.Windows11Extension
 local Windows11Extension = {}
 
+--- 通过该方法可以强制关闭 Windows 11 提供的窗口圆角样式  
+---   
+--- This method allow to force disable the window rounded corner style
+--- provided by Windows 11  
 ---@param allow boolean
 function Windows11Extension:setWindowCornerPreference(allow)
 end
 
 --------------------------------------------------------------------------------
---- known window instance
+--- 静态方法
+--- Static methods
 
+--- 获取主窗口  
+--- 
+--- Get instance of main window  
 ---@return lstg.Window
 function M.getMain()
 end
