6f1b05f4340ffbc7112c5766571940b9ba5baa27
璀境石 <git@chuikingshek.com>
2024-10-16 15:12:00 +0800

    adapt lstg.Color

--- release/luastg engine/lstg.Color.lua
+++ release/luastg engine/lstg.Color.lua
@@ -1,56 +1,104 @@
----=====================================
----fancy2d 颜色包装
----作者:Xiliusha
----邮箱:Xiliusha@outlook.com
----=====================================
+---@diagnostic disable: missing-return, duplicate-set-field
 
----@diagnostic disable: missing-return
+--------------------------------------------------------------------------------
+--- 类
+--- Class
 
-----------------------------------------
----lstg.Color
-
+--- 4 通道，每通道 8 位色深，32 位颜色  
+--- 
+--- 4 channels, 8-bits per channel, 32-bits color  
+--- 
+--- ```c
+--- union Color {
+---     struct ColorComponents {
+---         uint8_t b;
+---         uint8_t g;
+---         uint8_t r;
+---         uint8_t a;
+---     } components;
+---     uint32_t argb;
+--- };
+--- ```
 ---@class lstg.Color
-local f2dcolor = {
-	---alpha channel [[0~255]]
-	a = 0,
-	---red channel [[0~255]]
-	r = 0,
-	---green channel [[0~255]]
-	g = 0,
-	---blue channel [[0~255]]
-	b = 0,
-	---32bit color [[0x00000000~0xFFFFFFFF]]
-	argb = 0,
-	--[==[
-	---hue [[0~360]]
-	h = 0.0,
-	---saturation [[0~100]]
-	s = 0.0,
-	---value [[0~100]]
-	v = 0.0,
-	--]==]
-}
+local Color = {}
 
----返回颜色的4个分量，或者通过4个分量设置颜色
----@overload fun():number, number, number, number
----@overload fun(argb:number)
----@overload fun(a:number, r:number, g:number, b:number)
----@return number, number, number, number
-function f2dcolor:ARGB()
+--- Alpha（透明度）通道，0 到 255 的整数，值越小越透明  
+--- 
+--- Alpha channel, from 0 to 255 integer,
+--- smaller value is more transparent
+Color.a = 0
+
+--- 红色通道，0 到 255 的整数  
+--- 
+--- Red channel, from 0 to 255 integer  
+Color.r = 0
+
+--- 绿色通道，0 到 255 的整数  
+--- 
+--- Green channel, from 0 to 255 integer  
+Color.g = 0
+
+--- 蓝色通道，0 到 255 的整数  
+--- 
+--- Blue channel, from 0 to 255 integer  
+Color.b = 0
+
+--- 32 位整数，16 进制格式为 AARRGGBB    
+--- 
+--- 32-bits integer, in AARRGGBB hexadecimal format  
+Color.argb = 0x00000000
+
+--- 无参数调用，返回 a、r、g、b 通道值  
+--- 
+--- Call without parameters, returns a, r, g, b channel values  
+---@see lstg.Color.a
+---@see lstg.Color.r
+---@see lstg.Color.g
+---@see lstg.Color.b
+---@return number a
+---@return number r
+---@return number g
+---@return number b
+function Color:ARGB()
 end
 
---[==[
----返回颜色的4个分量，或者通过4个分量设置颜色，其中alpha取值为[[0~100]]
----@overload fun():number, number, number, number
----@overload fun(a:number, h:number, s:number, v:number)
----@return number, number, number, number
-function color:AHSV()
+---@see lstg.Color.argb
+---@param argb number
+function Color:ARGB(argb)
 end
---]==]
 
----构造颜色对象
----@overload fun(argb:number):lstg.Color
----@overload fun(a:number, r:number, g:number, b:number):lstg.Color
+---@see lstg.Color.a
+---@see lstg.Color.r
+---@see lstg.Color.g
+---@see lstg.Color.b
+---@param a number
+---@param r number
+---@param g number
+---@param b number
+function Color:ARGB(a, r, g, b)
+end
+
+--------------------------------------------------------------------------------
+--- 构造函数
+--- Constructor
+
+---@class lstg
+lstg = lstg or {}
+
+---@see lstg.Color.argb
+---@param argb number
+---@return lstg.Color
+function lstg.Color(argb)
+end
+
+---@see lstg.Color.a
+---@see lstg.Color.r
+---@see lstg.Color.g
+---@see lstg.Color.b
+---@param a number
+---@param r number
+---@param g number
+---@param b number
 ---@return lstg.Color
 function lstg.Color(a, r, g, b)
 end
