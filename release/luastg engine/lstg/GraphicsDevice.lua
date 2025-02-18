2a9aede7b2c8757f9f151e0b0806fe0a5361cec3
璀境石 <git@chuikingshek.com>
2025-02-18 22:43:14 +0800

    Texture2D

---
+++ release/luastg engine/lstg/GraphicsDevice.lua
@@ -0,0 +1,8 @@
+---@diagnostic disable: missing-return
+
+--------------------------------------------------------------------------------
+--- 接口
+--- Interface
+
+---@class lstg.GraphicsDevice : lstg.Unknown
+local M = {}
---
+++ release/luastg engine/lstg/GraphicsResource.lua
@@ -0,0 +1,8 @@
+---@diagnostic disable: missing-return
+
+--------------------------------------------------------------------------------
+--- 接口
+--- Interface
+
+---@class lstg.GraphicsResource : lstg.Unknown
+local M = {}
