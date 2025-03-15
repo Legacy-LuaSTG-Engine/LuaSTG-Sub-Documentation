d7f56e03c1a2bd813a9315186dc101cd21fe673c
璀境石 <git@chuikingshek.com>
2025-03-16 00:10:52 +0800

    SpriteRenderer setRect, setQuad, setTransform

--- release/luastg engine/lstg/SpriteRenderer.lua
+++ release/luastg engine/lstg/SpriteRenderer.lua
@@ -45,6 +45,51 @@ function SpriteRenderer:setLegacyBlendState(blend)
 end
 
 --------------------------------------------------------------------------------
+--- 辅助
+--- Helper
+
+---@param left number
+---@param right number
+---@param bottom number
+---@param top number
+function SpriteRenderer:setRect(left, right, bottom, top)
+end
+
+---@param x1 number
+---@param y1 number
+---@param x2 number
+---@param y2 number
+---@param x3 number
+---@param y3 number
+---@param x4 number
+---@param y4 number
+function SpriteRenderer:setQuad(x1, y1, x2, y2, x3, y3, x4, y4)
+end
+
+---@param x1 number
+---@param y1 number
+---@param z1 number
+---@param x2 number
+---@param y2 number
+---@param z2 number
+---@param x3 number
+---@param y3 number
+---@param z3 number
+---@param x4 number
+---@param y4 number
+---@param z4 number
+function SpriteRenderer:setQuad(x1, y1, z1, x2, y2, z2, x3, y3, z3, x4, y4, z4)
+end
+
+---@param x number
+---@param y number
+---@param rot number? default to 0
+---@param hscale number? default to 1.0
+---@param vscale number? default to hscale
+function SpriteRenderer:setTransform(x, y, rot, hscale, vscale)
+end
+
+--------------------------------------------------------------------------------
 --- 绘制
 --- Draw
 
