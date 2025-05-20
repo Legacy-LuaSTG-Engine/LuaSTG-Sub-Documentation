389c6eb30e19a378fa9e50cfb18a7f352dcc0334
璀境石 <git@chuikingshek.com>
2025-05-20 18:45:28 +0800

    FontManager

---
+++ release/luastg engine/lstg/FontManager.lua
@@ -0,0 +1,16 @@
+---@diagnostic disable: missing-return, unused-local
+
+---@class lstg.FontManager
+local FontManager = {}
+
+---@param path string
+---@return boolean
+function FontManager.registerFont(path)
+end
+
+---@param path string
+---@return boolean
+function FontManager.unregisterFont(path)
+end
+
+return FontManager
